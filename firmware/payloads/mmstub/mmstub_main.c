b4a89e11822336e0a0ad1d5b1cdab59bdcdbaf48
Yong Li <yong.li@intel.com>
2023-09-11 15:11:56 +0800

    Integrate with the PengZone code

--- firmware/payloads/mmstub/mmstub_main.c
+++ firmware/payloads/mmstub/mmstub_main.c
@@ -7,57 +7,6 @@
 
 #include "util.h"
 
-#define SBI_EXT_COVE  0x48923468
-#define SBI_COVE_SMM_EVENT_COMPLETE 0x82
-
-static inline void sbi_ecall_event_complete()
-{
-	SBI_ECALL_0(SBI_EXT_COVE, SBI_COVE_SMM_EVENT_COMPLETE);
-}
-
-#define SBI_EXT_MMSTUB  0x434F5649
-/* SBI function IDs for MMSTUB extension */
-#define SBI_COVE_SMM_INIT_COMPLETE		0x82
-#define SBI_COVE_SMM_EXIT		0x83
-
-static inline void sbi_ecall_init_complete()
-{
-	SBI_ECALL_0(SBI_EXT_MMSTUB, SBI_COVE_SMM_INIT_COMPLETE);
-}
-
-static inline void sbi_ecall_exit()
-{
-	SBI_ECALL_0(SBI_EXT_MMSTUB, SBI_COVE_SMM_EXIT);
-}
-
-typedef
-uint64_t
-(*PI_MM_RISCV_CPU_DRIVER_ENTRYPOINT) (
-	uint64_t  EventId,
-	uint64_t  CpuNumber,
-	uint64_t  NsCommBufferAddr
-	);
-
-static PI_MM_RISCV_CPU_DRIVER_ENTRYPOINT CpuDriverEntryPoint = NULL;
-void sm_smm_init(void *DriverEntryPoint)
-{
-	CpuDriverEntryPoint =
-		(PI_MM_RISCV_CPU_DRIVER_ENTRYPOINT)DriverEntryPoint;
-}
-
-uintptr_t sm_smm_communicate(uintptr_t a0, uintptr_t a1, uintptr_t a2)
-{
-	uintptr_t ret = 0;
-	// printk("[mmstub] debug line: before CpuDriverEntryPoint, a0: %lx, a1: %lx, a2: %lx\n",
-	// 	   a0, a1, a2);
-
-	ret = CpuDriverEntryPoint(a0, a1, a2);
-
-	// printk("[mmstub] debug line: after CpuDriverEntryPoint\n");
-
-	return ret;
-}
-
 typedef uint8_t UINT8;
 typedef uint16_t UINT16;
 typedef uint32_t UINT32;
@@ -95,10 +44,9 @@ typedef struct {
 	EFI_SECURE_PARTITION_CPU_INFO *CpuInfo;
 } EFI_SECURE_PARTITION_BOOT_INFO;
 
-// void (*_SMM_ModuleInit)(void *SharedBufAddress, int64_t SharedBufSize,
-// 			int64_t SharedCpuEntry, int64_t cookie);
-void (*_SMM_ModuleInit)(void *SharedBufAddress, void *SharedBufSize,
-			int64_t SharedCpuEntry, int64_t cookie);
+void (*_SMM_ModuleInit)(UINT64 CpuId, void *BootInfoAddress);
+void *sbi_memset(void *s, int c, size_t count);
+
 typedef struct {
 	EFI_SECURE_PARTITION_BOOT_INFO MmPayloadBootInfo;
 	EFI_SECURE_PARTITION_CPU_INFO MmCpuInfo[1];
@@ -106,24 +54,12 @@ typedef struct {
 
 EFI_SECURE_SHARED_BUFFER MmSharedBuffer;
 
-typedef struct {
-	UINT64 FuncId;
-	UINT64 Regs[2];
-	UINT64 Return;
-} EFI_COMMUNICATE_REG;
-
-// static int num = 0;
-
 void mmstub_main(unsigned long a0, unsigned long a1)
 {
 	printk("\n[mmstub] running\n");
 
-	// _SMM_ModuleInit =
-	// 	(void (*)(void *SharedBufAddress, int64_t SharedBufSize,
-	// 		  int64_t SharedCpuEntry, int64_t cookie))0x80C00000;
 	_SMM_ModuleInit =
-		(void (*)(void *SharedBufAddress, void *SharedBufSize,
-			  int64_t SharedCpuEntry, int64_t cookie))0x40C00000;
+		(void (*)(UINT64 CpuId, void *BootInfoAddress))0x40C00000;
 
 	MmSharedBuffer.MmPayloadBootInfo.Header.Version = 0x01;
 	MmSharedBuffer.MmPayloadBootInfo.SpMemBase	= 0x40C00000;
@@ -135,7 +71,8 @@ void mmstub_main(unsigned long a0, unsigned long a1)
 		0x42000000; // SpHeapBase + SpHeapSize + SpStackSize		
 	MmSharedBuffer.MmPayloadBootInfo.SpHeapBase =
 		0x40F00000; // SpMemBase + SpImageSize
-	MmSharedBuffer.MmPayloadBootInfo.SpNsCommBufBase = 0x7FE00000;
+	//MmSharedBuffer.MmPayloadBootInfo.SpNsCommBufBase = 0x4FE00000;
+	MmSharedBuffer.MmPayloadBootInfo.SpNsCommBufBase = 0x7FE000000;
 	MmSharedBuffer.MmPayloadBootInfo.SpSharedBufBase = 0x41F80000;
 	MmSharedBuffer.MmPayloadBootInfo.SpImageSize	 = 0x300000;
 	MmSharedBuffer.MmPayloadBootInfo.SpPcpuStackSize = 0x10000;
@@ -147,42 +84,8 @@ void mmstub_main(unsigned long a0, unsigned long a1)
 	MmSharedBuffer.MmCpuInfo[0].LinearId		 = 0;
 	MmSharedBuffer.MmCpuInfo[0].Flags		 = 0;
 	MmSharedBuffer.MmPayloadBootInfo.CpuInfo = MmSharedBuffer.MmCpuInfo;
-	// void *SharedBufAddress			 = &MmSharedBuffer;
-	// int64_t SharedBufSize			 = sizeof(MmSharedBuffer);
 
-	void *DriverEntryPoint			 = NULL;
-	int64_t SharedCpuEntry			 = (int64_t)&DriverEntryPoint;
-	int64_t cookie				 = 0;
-
-	printk("[mmstub] debug line: before _SMM_ModuleInit %x ### %x ### \n", _SMM_ModuleInit, MmSharedBuffer.MmPayloadBootInfo.SpStackBase);
-
-	// _SMM_ModuleInit(SharedBufAddress, SharedBufSize, SharedCpuEntry, cookie);
-	_SMM_ModuleInit(0, &MmSharedBuffer.MmPayloadBootInfo, SharedCpuEntry, cookie);
-
-	printk("[mmstub] ### Secure Monitor StandaloneMm Init ###\n");
-#if 0
-	sm_smm_init(DriverEntryPoint);
-
-	// sbi_ecall_init_complete();
-
-	while (TRUE) {
-        sbi_ecall_event_complete();
-		// sbi_ecall_console_puts("[mmstub] debug line: before sbi_ecall_wait_req\n");
-
-		EFI_COMMUNICATE_REG *comm_regs = (EFI_COMMUNICATE_REG *)0x80300000;
-		// printk("	**** [%s time%d] &comm_regs->FuncId: %p, comm_regs->FuncId: %lx\r\n",__func__, num, &comm_regs->FuncId, comm_regs->FuncId);
-		// printk("	**** [%s time%d] &comm_regs->Regs[0]: %p, comm_regs->Regs[0]: %lx\r\n",__func__, num, &comm_regs->Regs[0], comm_regs->Regs[0]);
-		// printk("	**** [%s time%d] &comm_regs->Regs[1]: %p, comm_regs->Regs[1]: %lx\r\n",__func__, num++, &comm_regs->Regs[1], comm_regs->Regs[1]);
-
-		uintptr_t ret = sm_smm_communicate(
-			comm_regs->FuncId, comm_regs->Regs[0], comm_regs->Regs[1]);
-
-		comm_regs->Return = ret;
-		// printk("	**** [%s time%d] &comm_regs->Return: %lx, comm_regs->Return: %lx\r\n",__func__, num++, &comm_regs->Return, comm_regs->Return);
-
-		// sbi_ecall_finish_req();
-		// sbi_ecall_console_puts("[mmstub] debug line: after sbi_ecall_finish_req\n");
-		// sbi_ecall_exit();
-	}
-#endif	
+	printk("[mmstub] debug line: before _SMM_ModuleInit\n");
+	sbi_memset((void *)(MmSharedBuffer.MmPayloadBootInfo.SpNsCommBufBase), 0, MmSharedBuffer.MmPayloadBootInfo.SpNsCommBufSize);
+	_SMM_ModuleInit(0, &MmSharedBuffer.MmPayloadBootInfo);
 }
