d69ba9f9a38bb185180e45778b27f7ab2a007405
Yong Li <yong.li@intel.com>
2024-02-28 13:58:41 +0800

    lib: utils/rpxy: Add SPD based RPXY driver for TEE
    This is a base RPXY driver to support dispatch
    TEE OSs. Initially, the optee_os is tested.
    
    Signed-off-by: Yong Li <yong.li@intel.com>

--- lib/utils/rpxy/objects.mk
+++ lib/utils/rpxy/objects.mk
@@ -12,3 +12,6 @@ libsbiutils-objs-$(CONFIG_FDT_RPXY) += rpxy/fdt_rpxy_drivers.o
 
 carray-fdt_rpxy_drivers-$(CONFIG_FDT_RPXY_MBOX) += fdt_rpxy_mbox
 libsbiutils-objs-$(CONFIG_FDT_RPXY_MBOX) += rpxy/fdt_rpxy_mbox.o
+
+carray-fdt_rpxy_drivers-$(CONFIG_FDT_RPXY_SPD) += fdt_rpxy_spd
+libsbiutils-objs-$(CONFIG_FDT_RPXY_SPD) += rpxy/fdt_rpxy_spd.o
