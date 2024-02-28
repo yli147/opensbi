1b8a39486c20fe39153cb881386db6c7522afbf4
Yong Li <yong.li@intel.com>
2024-02-28 13:52:48 +0800

    lib: sbi: Allocate rpxy shmmem for each domain
    Each domain need its own shared memory region.
    The SBI implementation is responsible for
    determining the accessible to each S-mode domain.
    
    Signed-off-by: Yong Li <yong.li@intel.com>

--- lib/sbi/sbi_init.c
+++ lib/sbi/sbi_init.c
@@ -350,11 +350,6 @@ static void __noreturn init_coldboot(struct sbi_scratch *scratch, u32 hartid)
 		sbi_hart_hang();
 	}
 
-	rc = sbi_rpxy_init(scratch);
-	if (rc) {
-		sbi_printf("%s: rpxy init failed (error %d)\n", __func__, rc);
-		sbi_hart_hang();
-	}
 
 	/*
 	 * Note: Finalize domains after HSM initialization so that we
@@ -366,6 +361,17 @@ static void __noreturn init_coldboot(struct sbi_scratch *scratch, u32 hartid)
 	if (rc) {
 		sbi_printf("%s: domain finalize failed (error %d)\n",
 			   __func__, rc);
+		sbi_hart_hang();
+	}
+
+	/*
+	 * Note: Each s-mode domain will need its own RPXY shared
+	 * memory region.
+	 * Note: Initialize the RPXY after domain finalize.
+	*/
+	rc = sbi_rpxy_init(scratch);
+	if (rc) {
+		sbi_printf("%s: rpxy init failed (error %d)\n", __func__, rc);
 		sbi_hart_hang();
 	}
 
